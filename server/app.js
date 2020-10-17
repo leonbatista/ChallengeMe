2ea7a8328a7b2421c44e54943f77f52468e94d02
leonbatista <55902667+leonbatista@users.noreply.github.com>
2020-10-16 19:55:05 -0700

    Database created and connected
    Connection made with mongoose

--- server/app.js
+++ server/app.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const { MONGOURI } = require("./keys");
 const PORT = 3000;
+
+mongoose.connect(MONGOURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => {
+  console.log("Connected to database");
+});
+mongoose.connection.on("error", (error) => {
+  console.log("Error connecting", error);
+});
 
 //This is a default route
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("New File");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About Page");
 });
 
 app.listen(PORT, () => {
